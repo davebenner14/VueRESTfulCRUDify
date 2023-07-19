2339edb3a4c822f58a91bc2ded625b0797da8bbe
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 16:12:07 -0400

    added grid look to DataList and create DataList css file

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <div v-for="item in dataList" :key="item.id">
-      <p>{{ item.name }}</p>
-      <button @click="deleteItem(item)">Delete</button>
-      <button @click="editItem(item)">Edit</button>
-    </div>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Actions</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Active</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in dataList" :key="item.id">
+        <td>
+          <button @click="editItem(item)">Edit</button>
+          <button @click="deleteItem(item)">Delete</button>
+        </td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.description }}</td>
+        <td>
+          <input type="checkbox" :checked="item.active" disabled />
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import "./DataList.css";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
