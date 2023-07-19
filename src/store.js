117f8ce51f2b35815866c719bfad82c2dcfa9e57
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 15:26:01 -0400

    add basic store file

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // state will be added here later
+  },
+  mutations: {
+    // mutations will be added here later
+  },
+  actions: {
+    // actions will be added here later
+  },
+  getters: {
+    // getters will be added here later
+  },
+});
