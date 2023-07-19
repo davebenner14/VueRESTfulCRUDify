34b01328803c4f18bf4662a288ac9847f73b4da5
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 15:31:24 -0400

    Update store to include CRUD operations

--- src/store.js
+++ src/store.js
@@ -1,19 +1,62 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    // state will be added here later
+    dataList: [],
   },
   mutations: {
-    // mutations will be added here later
+    SET_DATA(state, data) {
+      state.dataList = data;
+    },
+    ADD_DATA(state, data) {
+      state.dataList.push(data);
+    },
+    UPDATE_DATA(state, updatedData) {
+      const index = state.dataList.findIndex(
+        (data) => data.id === updatedData.id
+      );
+      if (index !== -1) {
+        state.dataList.splice(index, 1, updatedData);
+      }
+    },
+    DELETE_DATA(state, dataId) {
+      state.dataList = state.dataList.filter((data) => data.id !== dataId);
+    },
   },
   actions: {
-    // actions will be added here later
+    async fetchData({ commit }) {
+      const response = await axios.get("/api/data");
+      commit("SET_DATA", response.data);
+    },
+    async addData({ commit }, data) {
+      const response = await axios.post("/api/data", data);
+      commit("ADD_DATA", response.data);
+    },
+    async updateData({ commit }, updatedData) {
+      const response = await axios.put(
+        `/api/data/${updatedData.id}`,
+        updatedData
+      );
+      commit("UPDATE_DATA", response.data);
+    },
+    async deleteData({ commit }, id) {
+      await axios.delete(`/api/data/${id}`);
+      commit("DELETE_DATA", id);
+    },
   },
   getters: {
-    // getters will be added here later
+    dataList: (state) => state.dataList,
+    filteredData: (state) => (searchTerm) => {
+      if (!searchTerm) {
+        return state.dataList;
+      }
+      return state.dataList.filter((data) =>
+        data.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    },
   },
 });
