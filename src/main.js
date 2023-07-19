a47a5c7037db12d022bcf8368b7da6f29101007a
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 15:26:38 -0400

    Update Main.js file to include Vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
