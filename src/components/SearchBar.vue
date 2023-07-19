852b6aa319bcd840a4d5e2220feb302b06729896
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 15:30:43 -0400

    Add SearchBar component basic code

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="searchTerm" type="text" placeholder="Search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  watch: {
+    searchTerm() {
+      this.$emit("search", this.searchTerm);
+    },
+  },
+};
+</script>
