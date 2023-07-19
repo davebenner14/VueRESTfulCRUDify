b670e0f3f93442275570d789c4391dc3bf5477b0
David Benner <davidbenner@davids-mbp.lan>
2023-07-19 15:29:14 -0400

    Add DataList component with basic code

---
+++ src/components/DataList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="item in dataList" :key="item.id">
+      <p>{{ item.name }}</p>
+      <button @click="deleteItem(item.id)">Delete</button>
+      <button @click="editItem(item)">Edit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "DataList",
+  computed: {
+    ...mapGetters(["dataList"]),
+  },
+  methods: {
+    ...mapActions(["deleteData", "fetchData"]),
+    deleteItem(id) {
+      this.deleteData(id);
+    },
+    editItem(item) {
+      this.$emit("edit-item", item);
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
